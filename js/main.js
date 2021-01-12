064f04a5812094061a0e49444b604c2c5baa0858
Damien Hunter <a00014290@student.ait.ie>
2021-01-12 15:12:29 +0000

    Code Updates

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function calculate()
+{
+
+}
+
+function clear()
+{
+    document.getElementById("annual").value = '';
+    document.getElementById("paye").value = '';
+    document.getElementById("prsi").value = '';
+    document.getElementById("usc").value = '';
+    document.getElementById("payable").value = '';
+    document.getElementById("net").value = '';
+}
