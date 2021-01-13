b5dc869c8b7bf6805aafa3860ce1b10cfedcdf5f
Damien Hunter <a00014290@student.ait.ie>
2021-01-13 10:07:48 +0000

    Further updates to app redevelopment

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 function calculate()
 {
-
-}
-
-function clear()
-{
-    document.getElementById("annual").value = '';
-    document.getElementById("paye").value = '';
-    document.getElementById("prsi").value = '';
-    document.getElementById("usc").value = '';
-    document.getElementById("payable").value = '';
-    document.getElementById("net").value = '';
+    annualVal = document.getElementById("annual").value;
+    window.alert(annualVal);
 }
