f17292a8a4b87154aa62e9329403137cf41d42d5
Damien Hunter <a00014290@student.ait.ie>
2021-01-14 12:41:27 +0000

    Update to PRSI

--- js/main.js
+++ js/main.js
@@ -4,4 +4,7 @@ function calculate()
     status = document.getElementById("marital").value;
     ageStatus = document.getElementById("age").value;
     window.alert(status + " " + ageStatus + " " + annualVal);
+
+    prsiVal = (annualVal / 100) * 4;
+    document.getElementById("prsi").value = prsiVal;
 }
