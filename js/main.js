02ad70dd7a35f3abfdfe89b3dc22d5fa67b3ded5
Damien Hunter <a00014290@student.ait.ie>
2021-01-14 12:43:14 +0000

    Removed unnecessary code

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ function calculate()
     annualVal = document.getElementById("annual").value;
     status = document.getElementById("marital").value;
     ageStatus = document.getElementById("age").value;
-    window.alert(status + " " + ageStatus + " " + annualVal);
 
     prsiVal = (annualVal / 100) * 4;
     document.getElementById("prsi").value = prsiVal;
